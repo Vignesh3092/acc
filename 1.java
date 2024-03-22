524ac878aa36598972f6830023aeafb7c70da74f
BhavaniChandra4127 <139949962+BhavaniChandra4127@users.noreply.github.com>
2024-03-22 12:28:15 +0530

    Update 1.java

--- 1.java
+++ 1.java
@@ -1,18 +1,4 @@
-//1
-import java.util.*;
- class recursion {
-    static void rec(int a[],int i,int n,LinkedList<Integer> l)
-    {
-        if(i>n)
-        {
-            System.out.println(l);
-            return;
-        }
-        l.add(a[i]);
-        rec(a,i+1,n,l);
-        l.remove(a[i]);
-        rec(a,i+1,n,l);
-    }
+
     public static void main(String args[])
     {
         int arr[]={2,3,1};
